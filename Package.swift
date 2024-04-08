9d7eca4ea07d022b44319c7a1504b152ec289909
Gerry Wood <gerald.wood@sumup.com>
2024-04-08 11:58:11 -0400

    Release 0.0.2

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "TapiSDK",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(name: "TapiSDK", targets: ["TapiSDKTarget"]),
+    ], 
+    dependencies: [
+        .package(url: "https://github.com/Flight-School/AnyCodable", exact: "0.6.7"),
+        .package(url: "https://github.com/verygoodsecurity/vgs-collect-ios", exact: "1.15.2"),
+    ],
+    targets: [
+        .target(
+            name: "TapiSDKTarget",         // <--- new wrapper
+            dependencies: [
+                .product(name: "AnyCodable", package: "anycodable"),
+                .product(name: "VGSCollectSDK", package: "vgs-collect-ios"),
+                .target(name: "TapiSDK")    // <-- reference the actual binary target here
+            ],
+            publicHeadersPath: ""
+        ),
+        .binaryTarget(
+            name: "TapiSDK",
+            path: "TapiSDK.xcframework")
+    ]
+)
